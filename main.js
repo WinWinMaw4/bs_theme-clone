5f79d697d9fc80281252c508b236aa6571e5c4b1
WinWinMaw <mawinwinmaw4@gmail.com>
2022-07-16 16:54:57 +0630

    bootstrap theme clone

---
+++ main.js
@@ -0,0 +1,35 @@
+import './style.scss'
+import 'bootstrap'
+import Modal from "bootstrap/js/src/modal";
+import Swal from "sweetalert2";
+
+document.getElementById('st').addEventListener('click',function (){
+    Swal.fire('hello');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Modal from "bootstrap/js/src/modal";
+
+// import 'bootstrap/dist/js/bootstrap.min'
+// import 'bootstrap'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
